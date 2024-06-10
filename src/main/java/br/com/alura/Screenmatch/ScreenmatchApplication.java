7dcff65edda4d24f7b3a9f2760689e2cf575917e
Pedro Henrique <123213623+HenryPedro@users.noreply.github.com>
2024-06-10 19:16:49 -0300

    Inicio projeto spring

---
+++ src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.alura.Screenmatch;
+
+import br.com.alura.Screenmatch.Model.DadosSerie;
+import br.com.alura.Screenmatch.Service.ConsumoAPI;
+import br.com.alura.Screenmatch.Service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoapi = new ConsumoAPI();
+		var json = consumoapi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
